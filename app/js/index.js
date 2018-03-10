fd87ec183792f45ab74c91c654740e1acbc4f8b6
Yesterday17 <t@yesterday17.cn>
2018-03-10 15:32:05 +0800

    Simple Render works.

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,17 @@
+var md = require("markdown-it")();
+
 onload = function () {
     //
 }
 
+$("#markdown-input").bind("input propertychange", function(){
+    let text = $("#markdown-input").val();
+    
+    console.log(text);
+
+    let render = md.render(text);
+
+    console.log(render);
+
+    $("#render-column").replaceWith('<div id="render-column">' + render + '</div>');
+})
