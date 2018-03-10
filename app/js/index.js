43c4a8ac4ef630fc9be9991d980adea2313b809d
Yesterday17 <t@yesterday17.cn>
2018-03-10 15:02:41 +0800

    Remove Editor.md and add bootstraps

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,3 @@
-onload = function(){
-    var jquery = require('./jquery.min.js');
-    var editormd = require('./editormd.js');
-
-    var testEditor = editormd("test-editormd", {
-         width   : "90%",
-        height  : 640,
-        syncScrolling : "single"
-    });
-    console.log(testEditor);
+onload = function () {
+    //
 }
