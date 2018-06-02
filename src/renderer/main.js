62070bca56d58c43aa977987d166b471a3d031fc
Yesterday17 <t@yesterday17.cn>
2018-06-02 14:32:36 +0800

    feat: import vue-devtool to project

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -12,6 +12,12 @@ import store from './store'
 import 'codemirror/lib/codemirror.css'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+
+installExtension(VUEJS_DEVTOOLS)
+  .then((name) => console.log(`Added Extension:  ${name}`))
+  .catch((err) => console.log('An error occurred: ', err))
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
