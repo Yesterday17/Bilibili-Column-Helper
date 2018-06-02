0666c0762d42aab937917b8975ea8396f5e6fae3
Yesterday17 <t@yesterday17.cn>
2018-06-02 10:38:28 +0800

    feat: add subtype to meet bilibili's category

--- src/renderer/components/Passages.vue
+++ src/renderer/components/Passages.vue
@@ -8,18 +8,26 @@
       <el-dialog title="新建专栏" :visible.sync="dialogFormVisible">
         <el-form :model="form" :rules="rules" ref="newColumn">
           <el-form-item label="专栏标题（建议30字以内）：" label-width="220px" prop="name">
-            <el-input v-model="form.name" auto-complete="off"></el-input>
+            <el-input v-model="form.name" auto-complete="off" placeholder="请输入标题（建议30字以内）"></el-input>
           </el-form-item>
-          <el-form-item label="专栏分类：" label-width="220px" prop="typename">
-            <el-select v-model="form.typename" placeholder="请选择专栏分类">
-              <el-option label="动画" value="动画"></el-option>
-              <el-option label="游戏" value="游戏"></el-option>
-              <el-option label="影视" value="影视"></el-option>
-              <el-option label="生活" value="生活"></el-option>
-              <el-option label="兴趣" value="兴趣"></el-option>
-              <el-option label="轻小说" value="轻小说"></el-option>
-              <el-option label="科技" value="科技"></el-option>
-            </el-select>
+          <el-form-item label="专栏分类：" label-width="220px" required>
+            <el-col :span="11">
+              <el-select v-model="form.typename" placeholder="专栏分类" @change="changeSubtype" prop="typename">
+                <el-option label="动画" value="动画"></el-option>
+                <el-option label="游戏" value="游戏"></el-option>
+                <el-option label="影视" value="影视"></el-option>
+                <el-option label="生活" value="生活"></el-option>
+                <el-option label="兴趣" value="兴趣"></el-option>
+                <el-option label="轻小说" value="轻小说"></el-option>
+                <el-option label="科技" value="科技"></el-option>
+              </el-select>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-select v-model="form.subtype" placeholder="子分类" prop="subtype" @change="syncLabel" :label="label">
+                <el-option v-for="item in this.subtypeOptions" :key="item.value" :label="item.value" :value="item.value" :disabled="item.disabled"></el-option>
+              </el-select>
+            </el-col>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -45,10 +53,12 @@ export default {
     return {
       passages: [],
       dialogFormVisible: false,
+      label: '',
 
       form: {
         name: '',
         typename: '',
+        subtyppe: '',
         image: ''
       },
 
@@ -59,8 +69,19 @@ export default {
         ],
         typename: [
           { required: true, message: '请选择专栏分类', trigger: 'blur' }
+        ],
+        subtype: [
+          { required: true, message: '请选择专栏子分类', trigger: 'blur' }
         ]
-      }
+      },
+
+      subtypeOptions: [
+        {
+          value: '请选择专栏分类',
+          label: '请选择专栏分类',
+          disabled: true
+        }
+      ]
     }
   },
   methods: {
@@ -80,6 +101,93 @@ export default {
     },
     resetForm: function (form) {
       this.$refs[form].resetFields()
+    },
+    changeSubtype: function (selected) {
+      // Constants
+      const anime = [
+        {
+          label: '动漫杂谈',
+          value: '动漫杂谈',
+          disabled: false
+        },
+        {
+          label: '动漫资讯',
+          value: '动漫资讯',
+          disabled: false
+        },
+        {
+          label: '动画技术',
+          value: '动画技术',
+          disabled: false
+        }
+      ]
+      const game = [
+        {
+          label: '单机游戏',
+          value: '单机游戏',
+          disabled: false
+        },
+        {
+          label: '电子竞技',
+          value: '电子竞技',
+          disabled: false
+        },
+        {
+          label: '手机游戏',
+          value: '手机游戏',
+          disabled: false
+        },
+        {
+          label: '网络游戏',
+          value: '网络游戏',
+          disabled: false
+        },
+        {
+          label: '桌游棋牌',
+          value: '桌游棋牌',
+          disabled: false
+        }
+      ]
+      const film = [
+        {
+          label: '电影',
+          value: '电影',
+          disabled: false
+        },
+        {
+          label: '电视剧',
+          value: '电视剧',
+          disabled: false
+        },
+        {
+          label: '纪录片',
+          value: '纪录片',
+          disabled: false
+        },
+        {
+          label: '综艺',
+          value: '综艺',
+          disabled: false
+        }
+      ]
+
+      this.form.subtype = ''
+      this.subtypeOptions.splice(0, this.subtypeOptions.length)
+
+      switch (selected) {
+        case '动画':
+          this.subtypeOptions.push(...anime)
+          break
+        case '游戏':
+          this.subtypeOptions.push(...game)
+          break
+        case '影视':
+          this.subtypeOptions.push(...film)
+          break
+      }
+    },
+    syncLabel: function (selected) {
+      this.label = selected
     }
   },
   created () {
@@ -92,5 +200,11 @@ export default {
 .control {
   padding-top: 0;
   margin-bottom: 15px;
+}
+
+.line {
+  display: -webkit-flex;
+  display: flex;
+  justify-content: center;
 }
 </style>
