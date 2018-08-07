ec3ff93bf6ef3974defc464ab80cf756f21bc4fb
Yesterday17 <t@yesterday17.cn>
2018-08-07 15:20:50 +0800

    fix: poster is set to 'default' by default
    Users need to delete %appdata%/bilibili-column-helper/config.json to fit this fix.

--- src/renderer/store/modules/Config.js
+++ src/renderer/store/modules/Config.js
@@ -22,7 +22,7 @@ const mutations = {
     // state.xxx = s.get('')
     state.config.version = s.get('version', '1.0.0')
     state.config.renderer = s.get('renderer', 'biliZhuanlanMarkdownTool')
-    state.config.poster = s.get('poster', 'biliZhuanlanMarkdownTool')
+    state.config.poster = s.get('poster', 'default')
 
     state.config.cookies = s.get('cookies', [])
     state.config.cookie = s.get('cookie', '')
