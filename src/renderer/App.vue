3072b675fcf905413b1909fd00de48b9ed6e5b87
Yesterday17 <t@yesterday17.cn>
2018-08-23 00:50:53 +0800

    style: main ui almost finished

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <b-navbar id="app-nav" toggleable="sm" type="light" fixed="top" sticky>
+    <b-navbar id="app-nav" toggleable="sm" fixed="top" sticky>
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand href="#">
-        <img src="static/logo.png">
+      <b-navbar-brand class="bili-nav-header">
+        <img src="static/icon.png">
+        哔哩哔哩专栏助手
       </b-navbar-brand>
 
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav class="ml-auto">
           <b-button-group size="sm">
-            <b-btn class="btn-window" variant="outline-primary" @click="minimizeWindow"><img src="static/window/min-32.png"></b-btn>
-            <b-btn class="btn-window" variant="outline-primary" @click="reformWindow"><img :src="reformIcon"></b-btn>
-            <b-btn class="btn-window" variant="outline-primary" @click="closeWindow"><img src="static/window/close-32.png"></b-btn>
+            <b-btn class="btn-window" variant="outline-secondary" @click="minimizeWindow"><img src="static/window/min-32.png"></b-btn>
+            <b-btn class="btn-window" variant="outline-secondary" @click="reformWindow"><img :src="reformIcon"></b-btn>
+            <b-btn class="btn-window" variant="outline-secondary  " @click="closeWindow"><img src="static/window/close-32.png"></b-btn>
           </b-button-group>
         </b-navbar-nav>
       </b-collapse>
@@ -20,19 +21,19 @@
       <div id="sideButton" class="full-height">
         <ul>
           <li title="用户管理(Ctrl+Shift+U)" v-on:click="test('person')">
-            <octicon name="person" scale=2.2 width=50></octicon>
+            <octicon name="person" scale=2 width=50></octicon>
           </li>
           <li title="专栏管理(Ctrl+Shift+E)" v-on:click="test('book')">
-            <octicon name="book" scale=2.2 width=50></octicon>
+            <octicon name="book" scale=2 width=50></octicon>
           </li>
           <li title="创作中心(Ctrl+Shift+C)" v-on:click="test('pencil')">
-            <octicon name="pencil" scale=2.2 width=50></octicon>
+            <octicon name="pencil" scale=2 width=50></octicon>
           </li>
           <li title="用户设置(Ctrl+Shift+P)" v-on:click="test('gear')">
-            <octicon name="gear" scale=2.2 width=50></octicon>
+            <octicon name="gear" scale=2 width=50></octicon>
           </li>
           <li title="关于我们(Ctrl+Shift+A)" v-on:click="test('question')">
-            <octicon name="question" scale=2.2 width=50></octicon>
+            <octicon name="question" scale=2 width=50></octicon>
           </li>
         </ul>
       </div>
@@ -84,17 +85,37 @@ body {
 
 /* Global */
 .full-height {
-  height:100%;
+  height: 100%;
 }
 
 #app-nav {
   -webkit-app-region: drag; /* Make navbar dragable */
-  background-color: #409eff; /* Bilibili bule */
+  background-color: #666666;
   height: 58px; /* Specified height */
+}
+
+.bili-nav-header {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  font-size: 1.15rem !important;
+  color: #eee;
+}
+
+.bili-nav-header img{
+  padding-right: 1vw;
 }
 
 .btn-window {
   -webkit-app-region: no-drag; /* Make buttons clickable */
+  color: transparent !important;
+  border-color: transparent !important;
+}
+
+.btn-window:focus,
+.btn-window:hover,
+.btn-window:active {
+  box-shadow: 0 0 0 0;
+  background-color: #555555 !important;
+  border-color: transparent !important;
 }
 
 #app-body {
@@ -105,7 +126,7 @@ body {
 /* SideButton */
 #sideButton {
   width: 50px;
-  background-color: #6c757d;
+  background-color: #404040;
 }
 
 #sideButton ul {
@@ -114,16 +135,16 @@ body {
 
 #sideButton ul li {
   list-style-type: none;
-  padding-top: 10px;
-  padding-bottom: 5px;
+  padding-top: 12px;
+  padding-bottom: 6px;
 }
 
 #sideButton ul li svg {
-  color: #fff;
+  color: #bbbbbb;
   cursor: pointer;
 }
 
 #sideButton ul li svg:hover {
-  color: #000;
+  color: #fff;
 }
 </style>
