0150523b7869403439398d31999b09b99c6b4565
Yesterday17 <t@yesterday17.cn>
2018-10-28 17:07:08 +0800

    refactor: scss

--- src/renderer/components/edit.vue
+++ src/renderer/components/edit.vue
@@ -1,14 +1,29 @@
 <template>
-  <h1>Edit!</h1>
+  <div id="edit-body">
+    <div id="edit-no-passage" v-if="this.passage === undefined">2333</div>
+    <div id="edit-editor" v-else>{{this.passage}}</div>
+  </div>
 </template>
 <script>
 export default {
   data () {
-    return {}
+    return {
+      passage: undefined
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.passage = to.params.name
+    }
   }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+#edit-body {
+  #edit-no-passage {
+    margin: 0 auto;
+    text-align: center;
+  }
+}
 </style>
--- src/renderer/components/options.vue
+++ src/renderer/components/options.vue
@@ -17,5 +17,5 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 </style>
